40564e3b83df44e3f4c62c5749d7e61da7e8c501
elyzx <ellenchildren@icloud.com>
2021-06-28 22:21:21 +0200

    compeleted iterations 1-4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// GET Movies page
+router.get('/movies', (req, res, next) => {
+    MovieModel.find()
+        .then((movies) => {
+            res.render('movies', {movie: movies})
+        })
+        .catch(() => {
+            next('Failed to fetch movies')
+        })
+
+})
+
+router.get('/movie/:id', (req, res, next) => {
+    let dynamicMovieId = req.params.id
+
+    MovieModel.findById(dynamicMovieId)
+        .then((movie) => {
+            res.render('movie-detail.hbs', {movie})
+        })
+        .catch(() => {
+            next('Failed to find movie details')
+        })
+})
 
 module.exports = router;
